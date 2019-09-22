b0f1aec6f850030002c4a03139c0a712b8ec7df3
Karakatsanis Georgios <gkarakatsanis1@gmail.com>
2019-09-23 00:36:20 +0300

    added files

---
+++ Rocket/Rocket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rocket
+//
+//  Created by Karakatsanis Georgios on 22/09/2019.
+//  Copyright © 2019 Wrk.gr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
